cd0ec50a98ea4c77979dacea6615ac3f4382e8c7
birujung <amrita.deviayu@ui.ac.id>
2022-12-09 20:48:17 +0700

    Update FSM

--- FSM_Temp.vhd
+++ FSM_Temp.vhd
@@ -6,20 +6,22 @@ ENTITY FSM_Temp IS
   PORT (
     clk : IN STD_LOGIC;
     reset : IN STD_LOGIC;
-    temp_hot : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- "11"
-    temp_cold : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- "01"
-    temp_normal : IN STD_LOGIC_VECTOR(1 DOWNTO 0); --"10"
-    output : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+    temp_hot : IN STD_LOGIC; 
+    temp_cold : IN STD_LOGIC; 
+    temp_normal : IN STD_LOGIC; 
+    output : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)); -- "01" = cold, "10" = normal, "11" = hot
 END FSM_Temp;
 
 ARCHITECTURE behavioral OF FSM_Temp IS
-  TYPE state_type IS (state_normal, state_hot, state_cold);
+  -- ST0 = State Idle; ST1 = State Cold; ST2 = State Normal; ST3 = State Hot
+  TYPE state_type IS (ST0, ST1, ST2, ST3);
   SIGNAL current_state, next_state : state_type;
 BEGIN
+  -- The FSM State Transitions
   PROCESS (clk, reset)
   BEGIN
     IF (reset = '1') THEN
-      current_state <= state_normal;
+      current_state <= ST0;
     ELSIF (clk'event AND clk = '1') THEN
       current_state <= next_state;
     END IF;
@@ -29,40 +31,63 @@ BEGIN
   PROCESS (current_state, temp_hot, temp_cold, temp_normal)
   BEGIN
     CASE current_state IS
-      WHEN state_normal =>
-        IF (temp_normal = '0' AND temp_hot = '1' AND temp_cold = '0') THEN
-          next_state <= state_hot;
+      WHEN ST0 => -- State Idle
+        IF (temp_hot = '1') THEN
+          next_state <= ST3;
+        ELSIF (temp_cold = '1') THEN
+          next_state <= ST1;
+        ELSIF (temp_normal = '1') THEN
+          next_state <= ST2;
         ELSE
-          IF (temp_normal = '0' AND temp_hot = '0' AND temp_cold = '1') THEN
-            next_state <= state_cold;
-          ELSE
-            next_state <= state_normal;
+          next_state <= ST0;
         END IF;
-      WHEN state_hot =>
-        IF (temp_normal = '1' AND temp_hot = '0' AND temp_cold = '0') THEN
-          next_state <= state_normal;
+
+      WHEN ST1 => -- State Cold
+        IF (temp_hot = '1') THEN
+          next_state <= ST3;
+        ELSIF (temp_cold = '1') THEN
+          next_state <= ST1;
+        ELSIF (temp_normal = '1') THEN
+          next_state <= ST2;
         ELSE
-          IF (temp_normal = '0' AND temp_hot = '0' AND temp_cold = '1')
-            next_state <= state_cold;
-          ELSE
-            next_state <= state_hot;
+          next_state <= ST0;
         END IF;
-      WHEN state_cold =>
-        IF (temp_normal = '1' AND temp_hot = '0' AND temp_cold = '0') THEN
-          next_state <= state_normal;
+
+      WHEN ST2 => -- State Normal
+        IF (temp_hot = '1') THEN
+          next_state <= ST3;
+        ELSIF (temp_cold = '1') THEN
+          next_state <= ST1;
+        ELSIF (temp_normal = '1') THEN
+          next_state <= ST2;
         ELSE
-          IF (temp_normal = '0' AND temp_hot = '1' AND temp_cold = '0')
-            next_state <= state_hot;
-          ELSE
-            next_state <= state_cold;
-          END IF;
-      WHEN OTHERS =>
-        next_state <= state_normal;
+          next_state <= ST0;
+        END IF;
+
+      WHEN ST3 => -- State Hot
+        IF (temp_hot = '1') THEN
+          next_state <= ST3;
+        ELSIF (temp_cold = '1') THEN
+          next_state <= ST1;
+        ELSIF (temp_normal = '1') THEN
+          next_state <= ST2;
+        ELSE
+          next_state <= ST0;
+        END IF;
     END CASE;
   END PROCESS;
 
   -- Output logic
-  output <= "01" WHEN current_state = state_cold ELSE
-            "10" WHEN current_state = state_normal ELSE
-            "11" WHEN current_state = state_hot;
+  output <= "00";
+  PROCESS (current_state)
+  BEGIN
+    CASE current_state IS
+      WHEN ST0 =>
+        output <= "00"; -- State Idle
+      WHEN ST1 =>
+        output <= "01"; -- State Cold
+      WHEN ST2 =>
+        output <= "10"; -- State Normal
+      WHEN ST3 =>
+        output <= "11"; -- State Hot
 END behavioral;
--- FSM_Water.vhd
+++ FSM_Water.vhd
@@ -6,37 +6,39 @@ ENTITY FSM_Water IS
     PORT (
         H : IN STD_LOGIC; -- Sensor Tangan
         S : IN STD_LOGIC; -- Kran Air
+        Temp : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
         CLK : IN STD_LOGIC;
         sseg : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
         W : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)); -- Air keluar dalam "01" = cold, "10" = normal, "11" = hot
 END FSM_Water;
 
-ARCHITECTURE behavioral OF water_fsm IS
-    COMPONENT temp_fsm IS
+ARCHITECTURE behavioral OF FSM_Water IS
+    COMPONENT FSM_Temp IS
         PORT (
             clk : IN STD_LOGIC;
             reset : IN STD_LOGIC;
-            temp_hot : IN STD_LOGIC;
-            temp_cold : IN STD_LOGIC;
-            temp_normal : IN STD_LOGIC;
+            temp_hot : IN STD_LOGIC; 
+            temp_cold : IN STD_LOGIC; 
+            temp_normal : IN STD_LOGIC; 
             output : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
         );
     END COMPONENT;
+
     TYPE state_type IS (ST0, ST1, ST2, ST3, ST4);
     SIGNAL current_state, next_state : state_type;
-    SIGNAL inner_output : STD_LOGIC_VECTOR;
+    SIGNAL inner_output : STD_LOGIC_VECTOR(1 DOWNTO 0);
 BEGIN
     -- Instantiate the inner FSM
-    u1 : temp_fsm PORT MAP(
+    u1 : FSM_Temp PORT MAP(
         clk => clk,
         reset => reset,
-        temp_hot => temp_hot,
-        temp_cold => temp_cold,
-        temp_normal => temp_normal,
+        temp_hot => Temp,
+        temp_cold => Temp,
+        temp_normal => Temp,
         output => inner_output);
 
     -- The outer FSM logic
-    PROCESS (clk, reset)
+    PROCESS (clk, reset, next_state)
     BEGIN
         IF (reset = '1') THEN
             current_state <= ST0;
@@ -48,9 +50,10 @@ BEGIN
     -- Input logic
     H <= '0';
     S <= '0';
+    Temp <= "00";
 
     -- The FSM state transitions
-    PROCESS (current_state, inner_output)
+    PROCESS (current_state, inner_output, h, s)
     BEGIN
         CASE current_state IS
             WHEN ST0 => -- State Awal
@@ -60,30 +63,32 @@ BEGIN
 
             WHEN ST1 => -- State Normal
                 S <= '1';
-                IF (S = '1' AND inner_output = "001") THEN
+                IF (S = '1' AND inner_output = "01") THEN
                     next_state <= ST2; -- Ke State Cold
-                ELSIF (S = '1' AND inner_output = "011") THEN
+                ELSIF (S = '1' AND inner_output = "11") THEN
                     next_state <= ST3; -- Ke State Hot
-                END IF;
+            END IF;
 
             WHEN ST2 => -- State Cold
-                IF (S = '1' AND inner_output = "010") THEN
+                IF (S = '1' AND inner_output = "10") THEN
                     next_state <= ST1; -- Ke State Normal
-                ELSIF (S = '1' AND inner_output = "011") THEN
+                ELSIF (S = '1' AND inner_output = "11") THEN
                     next_state <= ST3; -- Ke State Hot
-                END IF;
+            END IF;
 
             WHEN ST3 => -- State Hot
-                IF (S = '1' AND inner_output = "010") THEN
+                IF (S = '1' AND inner_output = "10") THEN
                     next_state <= ST1; -- Ke State Normal
-                ELSIF (S = '1' AND inner_output = "001") THEN
+                ELSIF (S = '1' AND inner_output = "01") THEN
                     next_state <= ST2; -- Ke State Cold
-                END IF;
+            END IF;
 
             WHEN ST4 => -- State Akhir
                 IF (H = '0') THEN
                     next_state <= ST0;
-                END IF;
+            END IF;
+            ELSE =>
+
         END CASE;
     END PROCESS;
 
@@ -95,40 +100,39 @@ BEGIN
         CASE current_state IS
             WHEN ST0 => W <= "00" -- Tidak Keluar Air
                 AND sseg <= "000";
-            END IF;
 
             WHEN ST1 =>
-                IF (inner_output = "001") THEN
+                IF (inner_output = "01") THEN
                     W <= "01"; -- Air Dingin
                     sseg <= "001";
-                ELSIF (inner_output = "010") THEN
+                ELSIF (inner_output = "10") THEN
                     W <= "10"; -- Air Normal
                     sseg <= "010";
-                ELSIF (inner_output = "011") THEN
+                ELSIF (inner_output = "11") THEN
                     W <= "11"; -- Air Panas
                     sseg <= "011";
             END IF;
 
             WHEN ST2 =>
-                IF (inner_output = "001") THEN
+                IF (inner_output = "01") THEN
                     W <= "01"; -- Air Dingin
                     sseg <= "001";
-                ELSIF (inner_output = "010") THEN
+                ELSIF (inner_output = "10") THEN
                     W <= "10"; -- Air Normal
                     sseg <= "010";
-                ELSIF (inner_output = "011") THEN
+                ELSIF (inner_output = "11") THEN
                     W <= "11"; -- Air Panas
                     sseg <= "011";
             END IF;
 
             WHEN ST3 =>
-                IF (inner_output = "001") THEN
+                IF (inner_output = "01") THEN
                     W <= "01"; -- Air Dingin
                     sseg <= "001";
-                ELSIF (inner_output = "010") THEN
+                ELSIF (inner_output = "10") THEN
                     W <= "10"; -- Air Normal
                     sseg <= "010";
-                ELSIF (inner_output = "011") THEN
+                ELSIF (inner_output = "11") THEN
                     W <= "11"; -- Air Panas
                     sseg <= "011";
             END IF;
@@ -137,5 +141,5 @@ BEGIN
                 AND sseg <= "000";  
             END IF;
         END CASE;
-    END PROCESS;s
+    END PROCESS;
 END behavioral;
